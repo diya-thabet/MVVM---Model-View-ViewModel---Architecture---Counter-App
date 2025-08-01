bfeefa91eca43cba30f16400068c161d44737f35
diya-thabet <mohamaddhia@gmail.com>
2025-08-01 18:44:05 +0100

    working ui with right dependecies

--- app/src/main/java/com/fennec/mvvmcounter/TheCounterApp.kt
+++ app/src/main/java/com/fennec/mvvmcounter/TheCounterApp.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
