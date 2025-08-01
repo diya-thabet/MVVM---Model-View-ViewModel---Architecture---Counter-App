55d1e54aebad60ec15f772c30c6c0edbb32900c9
diya-thabet <mohamaddhia@gmail.com>
2025-08-01 18:42:23 +0100

    working ui

---
+++ app/src/main/java/com/fennec/mvvmcounter/TheCounterApp.kt
@@ -0,0 +1,62 @@
+package com.fennec.mvvmcounter
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun TheCounterApp(/*modifier: Modifier*/){
+    var counter = remember { mutableStateOf(0) }
+    fun increment(){
+        counter.value++
+    }
+
+    fun decrement(){
+        counter.value--
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Count: ${counter.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+        Row {
+            Button(onClick = {increment()} ) {
+                Text(
+                    text = "+"
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            Button(onClick = {decrement()}) {
+                Text(
+                    text = "-"
+                )
+            }
+        }
+    }
+}
